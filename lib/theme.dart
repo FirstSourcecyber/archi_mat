5408d29a273f5ea0884a844fb107aee68c6b9c57
salman78600 <imsalman5873@gmail.com>
2021-06-27 22:29:15 +0500

    updated

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,4 +5,7 @@ class AppTheme{
   Color white = Colors.white;
   Color purple =  Color(0xFFA12791);
   Color getstartedbg =  Color(0xFF708491);
+  Color yellow = Colors.yellow;
+  Color loginBtnColor = Color(0xFFB7B7B7);
+  Color virtualbg = Color(0xFF907070);
 }
